0941476ebd04c93f6171c6d93d3067d4db395600
Luiz Felipe <lfpereira323@gmail.com>
2023-03-14 01:28:44 -0300

    second commit

--- src/br/edu/univas/Ex4.java
+++ src/br/edu/univas/Ex4.java
@@ -11,16 +11,10 @@ public class Ex4 {
         System.out.println("digite o CPF:");
         String cpf = scan.next();
 
-        int[] multiplicadores = new int[9];
-        int[] multiplicadores2 = new int[10];
-        int inicioMultiplicadores = 2;
-
+        int[] multiplicadores = new int[10];
         int[] numerosCpf = new int[11];
-        int[] cpfDezDigitos = new int[10];
-        int[] cpfOnzeDigitos = new int[11];
-
+        int inicioMultiplicadores = 2;
         int qtdMult = multiplicadores.length;
-        int qtdMult2 = multiplicadores2.length;
         int somaCpf = 0;
         int dig;
         int dig2;
@@ -40,58 +34,45 @@ public class Ex4 {
 
         //preencher multiplicadores
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             multiplicadores[i] = inicioMultiplicadores;
             inicioMultiplicadores++;
         }
-        inicioMultiplicadores = 2;
 
-        //preencher multiplicadores2
-
-        for (int i = 0; i < 10; i++) {
-            multiplicadores2[i] = inicioMultiplicadores;
-
-            inicioMultiplicadores++;
-        }
-
-        //preencher cpfDezGigitos
+        //descobrir o primeiro digito
 
         for (int i = 0; i < 9; i++) {
-            somaCpf += numerosCpf[i] * multiplicadores[qtdMult-1];
+            somaCpf += numerosCpf[i] * multiplicadores[qtdMult-2];
             qtdMult--;
-            cpfDezDigitos[i] = numerosCpf[i];
         }
+        qtdMult = multiplicadores.length;
+
         if (somaCpf % 11 < 2) {
             dig = 0;
         } else {
             dig = 11 - (somaCpf % 11);
         }
-        cpfDezDigitos[9] = dig;
         somaCpf = 0;
 
-        //preencher cpfOnzeGigitos
+        //descobrir o segundo digito
 
         for (int i = 0; i < 10; i++) {
-            somaCpf += cpfDezDigitos[i] * multiplicadores2[qtdMult2-1];
-            qtdMult2--;
-            cpfOnzeDigitos[i] = cpfDezDigitos[i];
+            somaCpf += numerosCpf[i] * multiplicadores[qtdMult-1];
+            qtdMult--;
         }
+
         if (somaCpf % 11 < 2) {
             dig2 = 0;
         } else {
             dig2 = 11 - (somaCpf % 11);
         }
-        cpfOnzeDigitos[10] = dig2;
 
-        //dizer se o cpf é válido ou não
+        //dizer se é válido
 
-        for (int i = 0; i < 11; i++) {
-            if (numerosCpf[i] != cpfOnzeDigitos[i]) {
-                System.out.println("cpf inválido");
-                break;
-            } else if (i == 10 && numerosCpf[i] == cpfOnzeDigitos[i]){
-                System.out.println("cpf válido");
-            }
+        if (dig != numerosCpf[9] || dig2 != numerosCpf[10]) {
+            System.out.println("cpf inválido");
+        } else {
+            System.out.println("cpf válido");
         }
     }
 }
